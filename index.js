80144cb01cf859620de220dc56c68f3ef8f2aff3
ConnorYasin <connoryasin@gmail.com>
2025-09-09 14:57:49 -0600

    Exercise 2 Solved

--- index.js
+++ index.js
@@ -17,16 +17,20 @@ const users = [
 // broken test data for exercise 6
 
 // 1. Print out the names of each character in the console, then render them in the HTML list with id "names-list"
-users.forEach(DisplayNames);
+users.forEach(user => DisplayNames(user, "names-list"));
 
-function DisplayNames(user) {
-  const namesList = document.getElementById("names-list");
+function DisplayNames(user, elementId) {
+  const namesList = document.getElementById(elementId);
   const nameListItem = document.createElement("li");
   nameListItem.textContent = user.name;
   namesList.appendChild(nameListItem);
 }
 // 2. Print out the names of characters whose age is less than 40 in the console, then render them in the HTML list with id "young-characters-list"
-
+users.forEach(user => {
+  if (user.age < 40) {
+    DisplayNames(user, "young-characters-list");
+  }
+});
 // 3. Create a reusable function that takes any array and uses logic to render a list of character names in the HTML. Use this function to populate the list with id "function-list"
 
 // 4. Create a function that takes an array and an age threshold parameter. The function should only display characters whose age is below the given number. Render results in the list with id "age-filter-list"
