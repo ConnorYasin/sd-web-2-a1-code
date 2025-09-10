e9f520a81b24809d080da62b43b092c73eeb620e
ConnorYasin <connoryasin@gmail.com>
2025-09-09 18:18:18 -0600

    Moved brokenUsers

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ const users = [
 ];
 
 // broken test data for exercise 6
+const brokenUsers = [
+  { id: 1, name: "Luke Skywalker", age: 23 },
+  { id: 2, name: "Darth Vader", age: 45 },
+  { id: 3, name: "Princess Leia", age: 23 },
+  { id: 4, name: "Obi-Wan Kenobi", age: 57 },
+  { id: 5, age: 900 },
+  { id: 6, name: "Han Solo", age: 32 },
+  { id: 7, name: "Chewbacca", age: 234 },
+  { id: 8, name: "R2-D2", age: 33 },
+  { id: 9, age: 112 },
+  { id: 10, name: "Padmé Amidala", age: 27 },
+];
 
 // 1. Print out the names of each character in the console, then render them in the HTML list with id "names-list"
 users.forEach(user => displayNames(user, "names-list"));
@@ -36,6 +48,7 @@ function displayNames(item, elementId) {
     namesList.appendChild(nameListItem);
   }
 }
+
 // 2. Print out the names of characters whose age is less than 40 in the console, then render them in the HTML list with id "young-characters-list"
 users.forEach(user => {
   if (user.age < 40) {
@@ -58,20 +71,9 @@ function filterByAgeLessThanAndDisplayNames(array, ageThreshold, elementId){
 }
 
 filterByAgeLessThanAndDisplayNames(users, 100, "age-filter-list");
+
 // 5. Add error handling to your functions that will log an error message using console.error() if any object doesn't have a "name" property. Display any error messages in the div with id "error-messages"
 
 // 6. Test your error handling by creating a second array that's intentionally broken (missing name properties) and passing it to your functions. Verify that your error handling works correctly and displays errors in the div with id "broken-array-errors"
-const brokenUsers = [
-  { id: 1, name: "Luke Skywalker", age: 23 },
-  { id: 2, name: "Darth Vader", age: 45 },
-  { id: 3, name: "Princess Leia", age: 23 },
-  { id: 4, name: "Obi-Wan Kenobi", age: 57 },
-  { id: 5, age: 900 },
-  { id: 6, name: "Han Solo", age: 32 },
-  { id: 7, name: "Chewbacca", age: 234 },
-  { id: 8, name: "R2-D2", age: 33 },
-  { id: 9, age: 112 },
-  { id: 10, name: "Padmé Amidala", age: 27 },
-];
 
 displayArrayNames(brokenUsers, "broken-array-list");
